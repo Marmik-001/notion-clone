60a4c1f4f21ba0f2b7aa72fc9532c23104dd9f5e
Marmik-001 <marmikrshah@gmail.com>
2025-02-08 23:16:08 +0530

    updated

--- firebase.ts
+++ firebase.ts
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5oPWo0iZpPa4U0_NZMvYoZqgqIk5WcOA",
-  authDomain: "notion-clone-next.firebaseapp.com",
-  projectId: "notion-clone-next",
-  storageBucket: "notion-clone-next.firebasestorage.app",
-  messagingSenderId: "70806868193",
-  appId: "1:70806868193:web:b54d7eeab72e5361cce7a0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
-const db = getFirestore(app)
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
 
-export {db}
+export { db };
